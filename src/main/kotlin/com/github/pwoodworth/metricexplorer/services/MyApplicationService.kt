b7b20e1f24f0f11a185dd6cb4f200c07d2a2efc0
GitHub Action <action@github.com>
2020-12-19 02:49:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/pwoodworth/metricexplorer/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/pwoodworth/metricexplorer/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.pwoodworth.metricexplorer.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pwoodworth.metricexplorer.MyBundle
 
 class MyApplicationService {
 
